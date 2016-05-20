bb34984b87337660d921ea02b7ea45eafd86c00d
devkiang <********>
2016-05-20 17:39:50 +0800

    首页修改文件

---
+++ Classes/Scene/MainMenuScene.h
@@ -0,0 +1,23 @@
+
+#ifndef __MainMenu_SCENE_H__
+#define __MainMenu_SCENE_H__
+
+#include "LODConfig.h"
+#define MenuItemTag 10
+
+class MainMenuScene : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+    
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    void menuActionCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(MainMenuScene);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
